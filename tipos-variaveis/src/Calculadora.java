c214645c14036bf8ca6ad1d670ed5c5d6473bd66
Lucasra29 <lucasra_12@hotmail.com>
2024-04-28 23:32:59 -0300

    meu projeto conta banco

---
+++ tipos-variaveis/src/Calculadora.java
@@ -0,0 +1,15 @@
+
+
+public class Calculadora {
+    /**
+     * 
+     * @param numeroUm
+     * @param numeroDois
+     * @return
+     */
+    //javadoc -encoding UTF-8 -docencoding ISO-8859-1 -d ../docs src/*.java codigo terminal para documentacao html
+
+   public int somar(int numeroUm, int numeroDois){
+        return numeroUm+numeroDois;
+   }
+}
